5082c56ae22801fca83feb8a96e16d77127036f8
Anne <knight666+github@gmail.com>
2015-06-29 18:00:34 +0200

    Add virtual input device.

---
+++ Source/Input/VirtualInputManager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Base/Main.h"
+
+namespace Pitstop {
+
+	class RawInputJoystick;
+	class UsbDevice;
+	class VirtualInputDevice;
+
+	class VirtualInputManager
+	{
+
+	public:
+
+		VirtualInputManager();
+		~VirtualInputManager();
+
+		void bindJoystick(uint8_t index, RawInputJoystick& joystick);
+
+		void bindUsbDevice(uint8_t index, UsbDevice& usb);
+
+		void update(HANDLE device);
+
+	private:
+
+		QVector<VirtualInputDevice*> m_Devices;
+		QHash<HANDLE, VirtualInputDevice*> m_RawInputMapping;
+
+	}; // class VirtualInputManager
+
+}; // namespace Pitstop
