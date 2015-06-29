5f0efec12cd9e9a8b3da6980ae88c3b0629351d2
Purrminator <knight666@gmail.com>
2015-06-29 22:59:13 +0200

    Add bindings for digital input.

--- Source/Input/Process/InputProcessorFFBWheel.cpp
+++ Source/Input/Process/InputProcessorFFBWheel.cpp
@@ -4,11 +4,67 @@ namespace Pitstop {
 
 	void InputProcessorFFBWheel::createBindings()
 	{
-
+		addBinding("UpTrigger", InputType::Digital);
+		addBinding("DownTrigger", InputType::Digital);
+		addBinding("RightThumbButton", InputType::Digital);
+		addBinding("LeftThumbButton", InputType::Digital);
+		addBinding("GearBoxUp", InputType::Digital);
+		addBinding("GearBoxDown", InputType::Digital);
+		addBinding("DirectionalPadUp", InputType::Digital);
+		addBinding("DirectionalPadRight", InputType::Digital);
+		addBinding("DirectionalPadDown", InputType::Digital);
+		addBinding("DirectionalPadLeft", InputType::Digital);
 	}
 
 	bool InputProcessorFFBWheel::processDigital(USAGE identifier, bool pressed)
 	{
+		switch (identifier)
+		{
+
+		case 0x01:
+			m_Bindings["UpTrigger"].digitalValue = pressed;
+			break;
+
+		case 0x02:
+			m_Bindings["DownTrigger"].digitalValue = pressed;
+			break;
+
+		case 0x03:
+			m_Bindings["RightThumbButton"].digitalValue = pressed;
+			break;
+
+		case 0x04:
+			m_Bindings["LeftThumbButton"].digitalValue = pressed;
+			break;
+
+		case 0x05:
+			m_Bindings["GearBoxUp"].digitalValue = pressed;
+			break;
+
+		case 0x06:
+			m_Bindings["GearBoxDown"].digitalValue = pressed;
+			break;
+
+		case 0x07:
+			m_Bindings["DirectionalPadUp"].digitalValue = pressed;
+			break;
+
+		case 0x08:
+			m_Bindings["DirectionalPadRight"].digitalValue = pressed;
+			break;
+
+		case 0x09:
+			m_Bindings["DirectionalPadDown"].digitalValue = pressed;
+			break;
+
+		case 0x0A:
+			m_Bindings["DirectionalPadLeft"].digitalValue = pressed;
+			break;
+
+		default:
+			break;
+		}
+
 		return true;
 	}
 
