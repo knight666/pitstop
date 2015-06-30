d03a64c525ac1fc51faa50914787d5227a44d4b6
Purrminator <knight666@gmail.com>
2015-06-30 22:02:16 +0200

    Map and rename digital and analog input for wheel.

--- Source/Input/Process/InputProcessorFFBWheel.cpp
+++ Source/Input/Process/InputProcessorFFBWheel.cpp
@@ -4,16 +4,24 @@ namespace Pitstop {
 
 	void InputProcessorFFBWheel::createBindings()
 	{
-		addBinding("UpTrigger", InputType::Digital);
-		addBinding("DownTrigger", InputType::Digital);
-		addBinding("RightThumbButton", InputType::Digital);
+		addBinding("LeftGearShiftDown", InputType::Digital);
+		addBinding("RightGearShiftUp", InputType::Digital);
+		addBinding("LeftStickUp", InputType::Digital);
+		addBinding("LeftStickRight", InputType::Digital);
+		addBinding("LeftStickDown", InputType::Digital);
+		addBinding("LeftStickLeft", InputType::Digital);
+		addBinding("RightStickUp", InputType::Digital);
+		addBinding("RightStickRight", InputType::Digital);
+		addBinding("RightStickDown", InputType::Digital);
+		addBinding("RightStickLeft", InputType::Digital);
 		addBinding("LeftThumbButton", InputType::Digital);
-		addBinding("GearBoxUp", InputType::Digital);
-		addBinding("GearBoxDown", InputType::Digital);
-		addBinding("DirectionalPadUp", InputType::Digital);
-		addBinding("DirectionalPadRight", InputType::Digital);
-		addBinding("DirectionalPadDown", InputType::Digital);
-		addBinding("DirectionalPadLeft", InputType::Digital);
+		addBinding("RightThumbButton", InputType::Digital);
+		addBinding("GearShiftDown", InputType::Digital);
+		addBinding("GearShiftUp", InputType::Digital);
+
+		addBinding("Wheel", InputType::Analog);
+		addBinding("LeftFootPaddle", InputType::Analog);
+		addBinding("RightFootPaddle", InputType::Analog);
 	}
 
 	bool InputProcessorFFBWheel::processDigital(USAGE identifier, bool pressed)
@@ -22,11 +30,11 @@ namespace Pitstop {
 		{
 
 		case 0x01:
-			m_Bindings["UpTrigger"].digitalValue = pressed;
+			m_Bindings["RightGearShiftUp"].digitalValue = pressed;
 			break;
 
 		case 0x02:
-			m_Bindings["DownTrigger"].digitalValue = pressed;
+			m_Bindings["LeftGearShiftDown"].digitalValue = pressed;
 			break;
 
 		case 0x03:
@@ -38,27 +46,27 @@ namespace Pitstop {
 			break;
 
 		case 0x05:
-			m_Bindings["GearBoxUp"].digitalValue = pressed;
+			m_Bindings["GearShiftUp"].digitalValue = pressed;
 			break;
 
 		case 0x06:
-			m_Bindings["GearBoxDown"].digitalValue = pressed;
+			m_Bindings["GearShiftDown"].digitalValue = pressed;
 			break;
 
 		case 0x07:
-			m_Bindings["DirectionalPadUp"].digitalValue = pressed;
+			m_Bindings["RightStickUp"].digitalValue = pressed;
 			break;
 
 		case 0x08:
-			m_Bindings["DirectionalPadRight"].digitalValue = pressed;
+			m_Bindings["RightStickRight"].digitalValue = pressed;
 			break;
 
 		case 0x09:
-			m_Bindings["DirectionalPadDown"].digitalValue = pressed;
+			m_Bindings["RightStickDown"].digitalValue = pressed;
 			break;
 
 		case 0x0A:
-			m_Bindings["DirectionalPadLeft"].digitalValue = pressed;
+			m_Bindings["RightStickLeft"].digitalValue = pressed;
 			break;
 
 		default:
@@ -70,6 +78,33 @@ namespace Pitstop {
 
 	bool InputProcessorFFBWheel::processAnalog(USAGE identifier, LONG value)
 	{
+		switch (identifier)
+		{
+
+		case 0x30:
+			m_Bindings["Wheel"].analogValue = ((float)value / 2048.0f) - 1.0f;
+			break;
+
+		case 0x31:
+			m_Bindings["LeftFootPaddle"].analogValue = 1.0f - ((float)value / 510.0f);
+			break;
+
+		case 0x32:
+			m_Bindings["RightFootPaddle"].analogValue = 1.0f - ((float)value / 255.0f);
+			break;
+
+		case 0x39:
+			m_Bindings["LeftStickUp"].digitalValue = (value == 7 || value == 0 || value == 1);
+			m_Bindings["LeftStickRight"].digitalValue = (value == 1 || value == 2 || value == 3);
+			m_Bindings["LeftStickDown"].digitalValue = (value == 3 || value == 4 || value == 5);
+			m_Bindings["LeftStickLeft"].digitalValue = (value == 5 || value == 6 || value == 7);
+			break;
+
+		default:
+			break;
+
+		}
+
 		return true;
 	}
 
