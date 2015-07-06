3dd17afe504a8a4b916e0b07719683b5e0c5bacc
Quinten <knight666+github@gmail.com>
2015-07-06 14:32:48 +0200

    Moved joysticks tab to its own class and form.

--- Source/Application/MainWindow.cpp
+++ Source/Application/MainWindow.cpp
@@ -1,7 +1,5 @@
 #include "Application/MainWindow.h"
 
-#include <QtWidgets/QVBoxLayout>
-
 #include "Input/RawInputManager.h"
 
 namespace Pitstop {
@@ -12,42 +10,16 @@ namespace Pitstop {
 	{
 		m_Form.setupUi(this);
 
-		m_Form.scrollAreaWidgetContents->setLayout(new QVBoxLayout(m_Form.scrollAreaWidgetContents));
-
-		connect(
-			&m_RawInput, SIGNAL(signalJoystickConnected(RawInputJoystickPtr, bool)),
-			this, SLOT(slotJoystickConnected(RawInputJoystickPtr, bool)));
+		m_Form.tabJoysticks->setup(rawInput);
 	}
 
 	MainWindow::~MainWindow()
 	{
-		disconnect(
-			this, SLOT(slotJoystickConnected(RawInputJoystickPtr, bool)));
 	}
 
 	void MainWindow::bindJoystick(RawInputJoystick& joystick)
 	{
 		m_Form.bindingsList->bind(joystick);
-	}
-
-	void MainWindow::slotJoystickConnected(RawInputJoystickPtr joystick, bool connected)
-	{
-		QHash<QString, WidgetJoystickPtr>::iterator found = m_JoystickWidgets.find(joystick->getDevicePath());
-		if (found != m_JoystickWidgets.end())
-		{
-			return;
-		}
-
-		WidgetJoystickPtr widget(
-			new WidgetJoystick(joystick, m_Form.scrollAreaWidgetContents));
-
-		QVBoxLayout* layout = qobject_cast<QVBoxLayout*>(m_Form.scrollAreaWidgetContents->layout());
-		if (layout != nullptr)
-		{
-			layout->addWidget(widget.data());
-		}
-
-		m_JoystickWidgets.insert(joystick->getDevicePath(), widget);
 	}
 
 }; // namespace Pitstop
