72c1885236b25318466afaf0f97ae2784a29f2d5
Quinten <knight666+github@gmail.com>
2015-07-14 15:25:15 +0200

    Add container device and manager.

---
+++ Source/Input/Container/ContainerDevice.cpp
@@ -0,0 +1,13 @@
+#include "Input/Container/ContainerDevice.h"
+
+namespace Pitstop {
+
+	ContainerDevice::ContainerDevice()
+	{
+	}
+
+	ContainerDevice::~ContainerDevice()
+	{
+	}
+
+}; // namespace Pitstop
---
+++ Source/Input/Container/ContainerManager.cpp
@@ -0,0 +1,13 @@
+#include "Input/Container/ContainerManager.h"
+
+namespace Pitstop {
+
+	ContainerManager::ContainerManager()
+	{
+	}
+
+	ContainerManager::~ContainerManager()
+	{
+	}
+
+}; // namespace Pitstop
