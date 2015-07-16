9075cfa3d2c76c4c826dc696349ae52811e6396b
Quinten <quintenlansu@gmail.com>
2015-07-16 14:22:51 +0200

    Render treshold for trigger axes.

--- Source/Application/Widgets/WidgetSingleAxis.cpp
+++ Source/Application/Widgets/WidgetSingleAxis.cpp
@@ -10,6 +10,7 @@ namespace Pitstop {
 		, m_Minimum(0.0f)
 		, m_Maximum(1.0f)
 		, m_StepSize(0.1f)
+		, m_Treshold(0.0f)
 		, m_Value(0.0f)
 	{
 	}
@@ -29,11 +30,18 @@ namespace Pitstop {
 		}
 	}
 
-	void WidgetSingleAxis::setProperties(float minimum, float maximum, float stepSize /*= 0.1f*/)
+	void WidgetSingleAxis::setLimits(float minimum, float maximum, float stepSize /*= 0.1f*/)
 	{
 		m_Minimum = minimum;
 		m_Maximum = maximum;
 		m_StepSize = stepSize;
+
+		update();
+	}
+
+	void WidgetSingleAxis::setTreshold(float treshold)
+	{
+		m_Treshold = treshold;
 
 		update();
 	}
@@ -49,7 +57,10 @@ namespace Pitstop {
 		int step_center_y = event->rect().center().y();
 		int step_half_height = event->rect().height() / 4;
 
-		int value_offset = (int)(((m_Value - m_Minimum) / (m_Maximum - m_Minimum)) * event->rect().width());
+		int threshold_offset = (int)(((qMin(m_Value, m_Treshold) - m_Minimum) / (m_Maximum - m_Minimum)) * event->rect().width());
+		int treshold_x = (int)(((m_Treshold - m_Minimum) / (m_Maximum - m_Minimum)) * event->rect().width());
+
+		int value_offset = (int)(((qMax(m_Value - m_Treshold, m_Minimum) - m_Minimum) / (m_Maximum - m_Minimum)) * event->rect().width());
 		int value_half_height = event->rect().height() / 3;
 
 		QPainter painter(this);
@@ -58,16 +69,30 @@ namespace Pitstop {
 
 		painter.fillRect(event->rect(), QColor(255, 255, 255));
 
+		// Treshold
+
+		QRect treshold_rect;
+		treshold_rect.setTopLeft(
+			QPoint(
+				event->rect().left(),
+				event->rect().center().y() - value_half_height));
+		treshold_rect.setBottomRight(
+			QPoint(
+				event->rect().left() + threshold_offset,
+				event->rect().center().y() + value_half_height));
+
+		painter.fillRect(treshold_rect, QColor(196, 33, 33));
+
 		// Value
 
 		QRect value_rect;
 		value_rect.setTopLeft(
 			QPoint(
-				event->rect().left(),
+				event->rect().left() + threshold_offset,
 				event->rect().center().y() - value_half_height));
 		value_rect.setBottomRight(
 			QPoint(
-				event->rect().left() + value_offset,
+				event->rect().left() + threshold_offset + value_offset,
 				event->rect().center().y() + value_half_height));
 
 		painter.fillRect(value_rect, QColor(128, 128, 255));
@@ -96,17 +121,18 @@ namespace Pitstop {
 		{
 			int half_height = step_half_height;
 
-			if (i == step_count / 2)
-			{
-				half_height = event->rect().height() / 2;
-			}
-
 			painter.drawLine(
 				QPoint(step_x, step_center_y - half_height),
 				QPoint(step_x, step_center_y + half_height));
 
 			step_x += step_length;
 		}
+
+		// Treshold line
+
+		painter.drawLine(
+			QPoint(treshold_x, event->rect().top()),
+			QPoint(treshold_x, event->rect().bottom()));
 	}
 
 }; // namespace Pitstop
