09c899fb763ec929043aa5b86708358edbaaf51c
Quinten Lansu <quintenlansu@gmail.com>
2017-04-09 22:30:19 +0200

    Add input processor for Dark Tornado.

---
+++ Source/Input/Process/InputProcessorDarkTornado.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Base/Main.h"
+#include "Input/Process/InputProcessorBase.h"
+
+namespace Pitstop {
+
+	class InputProcessorDarkTornado
+		: public InputProcessorBase
+	{
+
+	public:
+		IMPLEMENT_INPUT_PROCESSOR(InputProcessorDarkTornado, 0x1A34, 0x0805);
+
+	public:
+		virtual void createBindings() override;
+
+		virtual bool processDigital(USAGE identifier, bool pressed) override;
+		virtual bool processAnalog(USAGE identifier, LONG value) override;
+
+	};
+
+}; // namespace Pitstop
