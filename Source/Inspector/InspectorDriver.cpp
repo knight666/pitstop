c2e82898ebd6d0b4fb16a42d0c592920d942d2e5
Quinten Lansu <knight666+github@gmail.com>
2017-04-12 22:14:30 +0200

    Track input values in driver.

--- Source/Inspector/InspectorDriver.cpp
+++ Source/Inspector/InspectorDriver.cpp
@@ -2,19 +2,55 @@
 
 namespace Pitstop {
 
+	InspectorDriver::InspectorDriver()
+	{
+	}
+
 	void InspectorDriver::createBindings()
 	{
-
+		if (!m_Tracking.isEmpty())
+		{
+			m_Tracking.clear();
+		}
 	}
 
 	bool InspectorDriver::processDigital(USAGE identifier, bool pressed)
 	{
+		trackValue(identifier, static_cast<LONG>(pressed));
+
 		return true;
 	}
 
 	bool InspectorDriver::processAnalog(USAGE identifier, LONG value)
 	{
+		trackValue(identifier, value);
+
 		return true;
+	}
+
+	void InspectorDriver::trackValue(USAGE identifier, LONG value)
+	{
+		QMap<LONG, int32_t>* tracked = nullptr;
+
+		auto found = m_Tracking.find(identifier);
+		if (found != m_Tracking.end())
+		{
+			tracked = &found.value();
+		}
+		else
+		{
+			m_Tracking.insert(identifier, QMap<LONG, int32_t>());
+			tracked = &m_Tracking.find(identifier).value();
+		}
+
+		auto found_counter = tracked->find(value);
+		if (found_counter == tracked->end())
+		{
+			tracked->insert(value, 0);
+			found_counter = tracked->find(value);
+		}
+
+		found_counter.value() += 1;
 	}
 
 };
--- Source/Inspector/InspectorDriver.h
+++ Source/Inspector/InspectorDriver.h
@@ -9,12 +9,22 @@ namespace Pitstop {
 		: public InputProcessorBase
 	{
 
+	public:
+
+		InspectorDriver();
+
 	private:
 
 		virtual void createBindings() override;
 
 		virtual bool processDigital(USAGE identifier, bool pressed) override;
 		virtual bool processAnalog(USAGE identifier, LONG value) override;
+
+		void trackValue(USAGE identifier, LONG value);
+
+	private:
+
+		QMap<USAGE, QMap<LONG, int32_t>> m_Tracking;
 
 	};
 
