4ce46b2b4b22e880d8d94af00ef9e0471f22a4b1
Quinten Lansu <quintenlansu@gmail.com>
2017-04-14 17:22:42 +0200

    Add value tracking to table.

--- Source/Inspector/Tracking/TrackingItem.cpp
+++ Source/Inspector/Tracking/TrackingItem.cpp
@@ -2,4 +2,47 @@
 
 namespace Pitstop {
 
+	TrackingItem::TrackingItem(USAGE identifier, InputType type)
+		: type(type)
+		, minimum(std::numeric_limits<int32_t>::max())
+		, maximum(std::numeric_limits<int32_t>::min())
+		, average(0.0f)
+		, median(0.0f)
+	{
+		switch (type)
+		{
+
+		case InputType::Digital:
+			name = QString("Digital%1").arg(identifier);
+			break;
+
+		case InputType::Analog:
+			name = QString("Analog%1").arg(identifier);
+			break;
+
+		case InputType::Axis:
+			name = QString("Axis%1").arg(identifier);
+			break;
+
+		default:
+			break;
+
+		}
+	}
+
+	void TrackingItem::add(int32_t value)
+	{
+		auto found = values.find(value);
+		if (found == values.end())
+		{
+			values.insert(value, 0);
+			found = values.find(value);
+		}
+
+		found.value() += 1;
+
+		minimum = std::min(minimum, value);
+		maximum = std::max(maximum, value);
+	}
+
 };
--- Source/Inspector/Tracking/TrackingItem.h
+++ Source/Inspector/Tracking/TrackingItem.h
@@ -5,11 +5,32 @@
 
 namespace Pitstop {
 
-	struct TrackingItem
+	class TrackingItem
 	{
+
+	public:
+
+		TrackingItem(USAGE identifier, InputType type);
+
+		void add(int32_t value);
+
+		const QString& getName() const { return name; }
+
+		int32_t getMinimum() const { return minimum; }
+		int32_t getMaximum() const { return maximum; }
+		float getAverage() const { return average; }
+		float getMedian() const { return median; }
+
+	public:
+
 		InputType type;
 		QString name;
 		QMap<LONG, int32_t> values;
+		int32_t minimum;
+		int32_t maximum;
+		float average;
+		float median;
+
 	};
 
 };
